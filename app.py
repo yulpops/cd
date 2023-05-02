f5c0502e02dd28f467296f658fbc965553051d65
MrDevil73 <evkaki31@gmail.com>
2023-05-02 17:25:20 +0400

    New Hello

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def validate_post_data(data: dict) -> bool:
 
 @app.route('/', methods=['GET'])
 def hello():
-    return 'this main page.!!!'
+    return 'Hello World!'
 
 @app.route('/api', methods=['GET', 'POST'])
 def api():
