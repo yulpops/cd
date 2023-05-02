463693c9ec8b787c62d6fbb58ac4609fd06b2128
yulpops <129699350+yulpops@users.noreply.github.com>
2023-05-02 23:25:58 +0400

    Add Page

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
 from flask import Flask
-from flask import request, jsonify
+from flask import request, jsonify,render_template
 
 app = Flask(__name__)
 app.debug = True
 
 
-def validate_post_data(data: dict) -> bool:
-    if not isinstance(data, dict):
-        return False
-    if not data.get('name') or not isinstance(data['name'], str):
-        return False
-    if data.get('age') and not isinstance(data['age'], int):
-        return False
-    return True
 
 @app.route('/', methods=['GET'])
 def hello():
@@ -29,15 +21,11 @@ def api():
             }
     :return:
     """
-    if request.method == 'GET':
-        return jsonify({'status': 'test'})
-    elif request.method == 'POST':
-        if validate_post_data(request.json):
-            return jsonify({'status': 'OK'})
-        else:
-            return jsonify({'status': 'Have problems'}),400
 
-@app.route("/test",)
+@app.route("/hello",)
+def page():
+    return render_template("hello.html")
+
 def main():
     app.run(port=8000)
 
