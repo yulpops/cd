29503c7419fda48bd1815acd088410e6319c5a14
yulpops <129699350+yulpops@users.noreply.github.com>
2023-06-05 22:52:09 +0400

    Добавление файла

---
+++ tst.py
@@ -0,0 +1,12 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_home_page(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Hello, World!" in response.data
