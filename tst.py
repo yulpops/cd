dc68b3d7812a98dc3f5a7a381e6d456d4daff38a
Yulya P <popsulsu@mail.ru>
2023-06-06 18:36:38 +0400

    замена заднего фона

--- tst.py
+++ tst.py
@@ -6,7 +6,7 @@ def client():
     with app.test_client() as client:
         yield client
 
-def test_home_page(client):
-    response = client.get('/')
-    assert response.status_code == 200
-    assert b"Last Hello World !!" in response.data
+def test_home_page(client): #создаём виртуального клиента
+    response = client.get('/') #делаем запрос на главную страницу
+    assert response.status_code == 200 #при получении кода ответ 200
+    assert b"Last Hello World !!" in response.data 
